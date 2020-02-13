edcf9e83cabe93df02ff08c2f237e7cf1d74ecd3
Uriel Salischiker <uriel@urielsalis.me>
2020-02-13 20:01:05 +0100

    Make function pure

--- src/main/kotlin/io/github/mojira/arisa/modules/AttachmentModule.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/AttachmentModule.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import arrow.core.extensions.fx
 import arrow.core.left
 import arrow.core.right
+import arrow.syntax.function.partially1
 import net.rcarz.jiraclient.Attachment
 
 
@@ -15,26 +16,27 @@ class AttachmentModule(
 ) : Module<AttachmentModuleRequest> {
 
     override fun invoke(request: AttachmentModuleRequest): Either<ModuleError, ModuleResponse> = Either.fx {
-        val blackListedAttachments = request.attachments.filter(::endsWithBlacklistedExtensions)
+        val endsWithBlacklistedExtensionAdapter = ::endsWithBlacklistedExtensions.partially1(extensionBlackList)
+        val blackListedAttachments = request.attachments.filter(endsWithBlacklistedExtensionAdapter)
         assertNotEmpty(blackListedAttachments).bind()
         deleteAttachmentsAdapter(deleteAttachment, blackListedAttachments).bind()
     }
+}
 
-    private fun endsWithBlacklistedExtensions(attachment: Attachment) =
-        extensionBlackList.any { attachment.contentUrl.endsWith(it) }
+private fun endsWithBlacklistedExtensions(extensionBlackList: List<String>, attachment: Attachment) =
+    extensionBlackList.any { attachment.contentUrl.endsWith(it) }
 
-    private fun deleteAttachmentsAdapter(
-        deleteAttachment: (Attachment) -> Either<Throwable, Unit>,
-        blackListedAttachments: List<Attachment>
-    ) = if (blackListedAttachments.map(deleteAttachment).any { it.isLeft() }) {
-        FailedModuleResponse(listOf()).left()
-    } else {
-        Unit.right()
-    }
+private fun deleteAttachmentsAdapter(
+    deleteAttachment: (Attachment) -> Either<Throwable, Unit>,
+    blackListedAttachments: List<Attachment>
+) = if (blackListedAttachments.map(deleteAttachment).any { it.isLeft() }) {
+    FailedModuleResponse(listOf()).left()
+} else {
+    Unit.right()
+}
 
-    private fun assertNotEmpty(blackListedAttachments: List<Attachment>) = if (blackListedAttachments.isEmpty()) {
-        OperationNotNeededModuleResponse.left()
-    } else {
-        Unit.right()
-    }
+private fun assertNotEmpty(blackListedAttachments: List<Attachment>) = if (blackListedAttachments.isEmpty()) {
+    OperationNotNeededModuleResponse.left()
+} else {
+    Unit.right()
 }

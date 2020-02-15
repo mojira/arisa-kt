1718b478dc0515b43602f05452f3a2f2a671ddb6
Uriel Salischiker <uriel.salischiker@zopa.com>
2020-02-15 17:18:06 +0100

    passing test - should return OperationNotNeededModuleResponse when there is no resolution

--- src/main/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModule.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModule.kt
@@ -1,12 +1,17 @@
 package io.github.mojira.arisa.modules
 
 import arrow.core.Either
+import arrow.core.left
 import net.rcarz.jiraclient.Resolution
 
 data class ReopenAwaitingModuleRequest(val resolution: Resolution?)
 
 class ReopenAwaitingModule : Module<ReopenAwaitingModuleRequest> {
     override fun invoke(request: ReopenAwaitingModuleRequest): Either<ModuleError, ModuleResponse> {
+        if (request.resolution == null) {
+            return OperationNotNeededModuleResponse.left()
+        }
+        
         TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }

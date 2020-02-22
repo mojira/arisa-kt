9beb6c015b2c80cd78f3298a27dae2f637c403b2
Uriel Salischiker <urielsalis@users.noreply.github.com>
2020-02-22 11:54:19 +0100

    Piracy module (#2)

--- src/main/kotlin/io/github/mojira/arisa/ArisaMain.kt
+++ src/main/kotlin/io/github/mojira/arisa/ArisaMain.kt
@@ -1,14 +1,15 @@
 package io.github.mojira.arisa
 
 import arrow.core.Either
-import arrow.syntax.function.curried
 import arrow.syntax.function.partially1
 import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.source.yaml
+import io.github.mojira.arisa.infrastructure.addComment
 import io.github.mojira.arisa.infrastructure.config.Arisa
 import io.github.mojira.arisa.infrastructure.connectToJira
 import io.github.mojira.arisa.infrastructure.deleteAttachment
 import io.github.mojira.arisa.infrastructure.reopenIssue
+import io.github.mojira.arisa.infrastructure.resolveAsInvalid
 import io.github.mojira.arisa.infrastructure.updateCHK
 import io.github.mojira.arisa.modules.AttachmentModule
 import io.github.mojira.arisa.modules.AttachmentModuleRequest
@@ -17,6 +18,8 @@ import io.github.mojira.arisa.modules.CHKModuleRequest
 import io.github.mojira.arisa.modules.FailedModuleResponse
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
+import io.github.mojira.arisa.modules.PiracyModule
+import io.github.mojira.arisa.modules.PiracyModuleRequest
 import io.github.mojira.arisa.modules.ReopenAwaitingModule
 import io.github.mojira.arisa.modules.ReopenAwaitingModuleRequest
 import net.rcarz.jiraclient.Issue
@@ -65,14 +68,24 @@ fun main() {
 }
 
 fun initModules(config: Config, jiraClient: JiraClient): (Issue) -> List<Either<ModuleError, ModuleResponse>> {
-    val attachmentModule = AttachmentModule(
-        ::deleteAttachment.partially1(jiraClient),
-        config[Arisa.Modules.Attachment.extensionBlacklist].split(",")
-    )
-    val chkModule = CHKModule(::updateCHK.curried()(config)(jiraClient))
-    val reopenAwaitingModule = ReopenAwaitingModule(::reopenIssue)
 
     return { issue: Issue ->
+        val attachmentModule = AttachmentModule(
+            ::deleteAttachment.partially1(jiraClient),
+            config[Arisa.Modules.Attachment.extensionBlacklist].split(",")
+        )
+        val chkModule = CHKModule(
+            ::updateCHK.partially1(issue).partially1(config[Arisa.CustomFields.chkField])
+        )
+        val reopenAwaitingModule = ReopenAwaitingModule(
+            ::reopenIssue.partially1(issue)
+        )
+        val piracyModule = PiracyModule(
+            ::resolveAsInvalid.partially1(issue),
+            ::addComment.partially1(issue).partially1(config[Arisa.Modules.Piracy.piracyMessage]),
+            config[Arisa.Modules.Piracy.piracySignatures].split(",")
+        )
+
         listOf(
             attachmentModule(AttachmentModuleRequest(issue.attachments)),
             chkModule(
@@ -87,9 +100,11 @@ fun initModules(config: Config, jiraClient: JiraClient): (Issue) -> List<Either<
                     issue.resolution,
                     issue.getField("created") as Date,
                     issue.getField("updated") as Date,
-                    issue.comments,
-                    issue
+                    issue.comments
                 )
+            ),
+            piracyModule(
+                PiracyModuleRequest(issue.getField("environment") as String?, issue.summary, issue.description)
             )
         )
     }

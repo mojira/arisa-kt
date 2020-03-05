3506af97aed0a9c42ffe00998a670d3c7eae6baa
urielsalis <uriel@urielsalis.me>
2020-03-05 21:16:12 +0100

    Add shadow mode

--- src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
+++ src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
@@ -3,6 +3,7 @@ package io.github.mojira.arisa.infrastructure.config
 import com.uchuhimo.konf.ConfigSpec
 
 object Arisa : ConfigSpec() {
+    val shadow by optional(false)
     object Credentials : ConfigSpec() {
         val username by required<String>()
         val password by required<String>()

8d4f6da26a76d5edd93c4c240bacbd3b71a2b464
Uriel Salischiker <urielsalis@users.noreply.github.com>
2020-03-09 15:50:07 +0100

    Project whitelist in each module (#4)
    * Rough implementation of per project whitelist. To be refined later
    
    * Remove .kotlinttest
    
    * Whitelist per project
    
    Co-authored-by: Uriel Salischiker <uriel.salischiker@zopa.com>

--- src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
+++ src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
@@ -21,11 +21,15 @@ object Arisa : ConfigSpec() {
     }
 
     object Modules : ConfigSpec() {
-        object Attachment : ConfigSpec() {
+        open class ModuleConfigSpec : ConfigSpec() {
+            val whitelist by optional("MC,MCTEST,MCPE,MCAPI,MCL,MC")
+        }
+
+        object Attachment : ModuleConfigSpec() {
             val extensionBlacklist by optional("jar,exe,com,bat,msi,run,lnk,dmg")
         }
 
-        object Piracy : ConfigSpec() {
+        object Piracy : ModuleConfigSpec() {
             val piracyMessage by optional(
                 "You are currently using a *non-authorized* version of Minecraft." +
                     " If you wish to purchase the full game, please visit the [Minecraft Store|https://minecraft.net/store].\\r\\n" +
@@ -33,5 +37,9 @@ object Arisa : ConfigSpec() {
             )
             val piracySignatures by optional("Minecraft Launcher null,Bootstrap 0,Launcher: 1.0.10  (bootstrap 4),Launcher: 1.0.10  (bootstrap 5),Launcher 3.0.0,Launcher: 3.1.0,Launcher: 3.1.1,Launcher: 3.1.4,1.0.8,uuid sessionId,auth_access_token,windows-\${arch},keicraft,keinett,nodus,iridium,mcdonalds,uranium,nova,divinity,gemini,mineshafter,Team-NeO,DarkLBP,Launcher X,PHVL,Pre-Launcher v6,LauncherFEnix,TLauncher")
         }
+
+        object CHK : ModuleConfigSpec()
+
+        object ReopenAwaiting : ModuleConfigSpec()
     }
 }

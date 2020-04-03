e329f180d5df8902c83f259694250cf7c697d8d5
Uriel Salischiker <uriel.salischiker@zopa.com>
2020-04-03 10:00:00 +0200

    Run in all projects

--- src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
+++ src/main/kotlin/io/github/mojira/arisa/infrastructure/config/Arisa.kt
@@ -4,13 +4,14 @@ import com.uchuhimo.konf.ConfigSpec
 
 object Arisa : ConfigSpec() {
     val shadow by optional(false)
+
     object Credentials : ConfigSpec() {
         val username by required<String>()
         val password by required<String>()
     }
 
     object Issues : ConfigSpec() {
-        val projects by optional(listOf("MC", "MCTEST", "MCPE", "MCAPI", "MCL", "MC", "MCE"))
+        val projects by optional(listOf("MC", "MCTEST", "MCPE", "MCAPI", "MCL", "MCD", "MCE", "BDS"))
         val url by optional("https://bugs.mojang.com/")
         val checkInterval by optional(10L)
     }
@@ -24,7 +25,7 @@ object Arisa : ConfigSpec() {
 
     object Modules : ConfigSpec() {
         open class ModuleConfigSpec : ConfigSpec() {
-            val whitelist by optional(listOf("MC", "MCTEST", "MCPE", "MCAPI", "MCL"))
+            val whitelist by optional(listOf("MC", "MCTEST", "MCPE", "MCAPI", "MCL", "MCD", "MCE", "BDS"))
         }
 
         object Attachment : ModuleConfigSpec() {
@@ -34,8 +35,8 @@ object Arisa : ConfigSpec() {
         object Piracy : ModuleConfigSpec() {
             val piracyMessage by optional(
                 "You are currently using a *non-authorized* version of Minecraft." +
-                    " If you wish to purchase the full game, please visit the [Minecraft Store|https://minecraft.net/store].\r\n" +
-                    "We will not provide support for pirated versions of the game, these versions are modified and may contain malware."
+                        " If you wish to purchase the full game, please visit the [Minecraft Store|https://minecraft.net/store].\r\n" +
+                        "We will not provide support for pirated versions of the game, these versions are modified and may contain malware."
             )
             val piracySignatures by optional(
                 listOf(
@@ -81,7 +82,7 @@ object Arisa : ConfigSpec() {
                 "Please *do not* mark _unreleased versions_ as affected.\r\nYou don't have access to them yet."
             )
         }
-
+        
         object CHK : ModuleConfigSpec()
 
         object ReopenAwaiting : ModuleConfigSpec()
@@ -100,18 +101,20 @@ object Arisa : ConfigSpec() {
         object Crash : ModuleConfigSpec() {
             val maxAttachmentAge by optional(30)
             val crashExtensions by optional(listOf("txt", "log"))
-            val duplicateMessage by optional("Duplicate of {DUPLICATE} -- " +
-                    "If you have not, please use the [search function|https://bugs.mojang.com/issues/] in the future, " +
-                    "to see if your bug has already been submitted.\r\n" +
-                    "For technical support, please use the " +
-                    "[Mojang Support Center|http://help.mojang.com/customer/portal/articles/364794-where-can-i-find-more-help-and-technical-support-]."
+            val duplicateMessage by optional(
+                "Duplicate of {DUPLICATE} -- " +
+                        "If you have not, please use the [search function|https://bugs.mojang.com/issues/] in the future, " +
+                        "to see if your bug has already been submitted.\r\n" +
+                        "For technical support, please use the " +
+                        "[Mojang Support Center|http://help.mojang.com/customer/portal/articles/364794-where-can-i-find-more-help-and-technical-support-]."
             )
-            val moddedMessage by optional("This ticket is _invalid_ as it relates to a modified or third-party client, server, or launcher.\r\n" +
-                    "* Any non-standard client/server/launcher build needs to be taken up with the appropriate team, not Mojang.\r\n" +
-                    "* Any plugin issues need to be addressed to the creator of the plugin or resource pack.\r\n" +
-                    "* This site is for addressing issues related to the *base unmodded Minecraft*; " +
-                    "any modded system _invalidates_ your ticket, unless the behavior can be reproduced without mods.\r\n* " +
-                    "Additionally, if you have problems on large-scale modded servers, please report it to their site. It's probably not a bug in Minecraft."
+            val moddedMessage by optional(
+                "This ticket is _invalid_ as it relates to a modified or third-party client, server, or launcher.\r\n" +
+                        "* Any non-standard client/server/launcher build needs to be taken up with the appropriate team, not Mojang.\r\n" +
+                        "* Any plugin issues need to be addressed to the creator of the plugin or resource pack.\r\n" +
+                        "* This site is for addressing issues related to the *base unmodded Minecraft*; " +
+                        "any modded system _invalidates_ your ticket, unless the behavior can be reproduced without mods.\r\n* " +
+                        "Additionally, if you have problems on large-scale modded servers, please report it to their site. It's probably not a bug in Minecraft."
             )
 
             val duplicates by optional(

9139426847d34547ac1cc0276264de3e001e0c9f
NeunEinser <daniel30797@gmail.com>
2020-04-23 09:02:33 +0200

    Feature/dont reopen after staff edit (#143)
    * Don't reopen awaiting reponse caused by a staff comment or a restricted comment
    
    * ktlint
    
    * fix build
    
    Co-authored-by: Uriel Salischiker <urielsalis@users.noreply.github.com>

--- src/main/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModule.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModule.kt
@@ -6,10 +6,16 @@ import arrow.core.left
 import arrow.core.right
 import java.time.Instant
 
-class ReopenAwaitingModule : Module<ReopenAwaitingModule.Request> {
+class ReopenAwaitingModule(
+    private val blacklistedRoles: List<String>,
+    private val blacklistedVisibilities: List<String>
+) : Module<ReopenAwaitingModule.Request> {
     data class Comment(
         val updated: Long,
-        val created: Long
+        val created: Long,
+        val visibilityType: String?,
+        val visibilityValue: String?,
+        val authorGroups: List<String>?
     )
 
     data class Request(
@@ -29,20 +35,33 @@ class ReopenAwaitingModule : Module<ReopenAwaitingModule.Request> {
             assertUpdateWasNotCausedByEditingComment(
                 updated.toEpochMilli(), lastComment.updated, lastComment.created
             ).bind()
+            assertCommentWasNotAddedByABlacklistedRole(lastComment.authorGroups).bind()
+            assertCommentIsNotRestrictedToABlacklistedLevel(lastComment.visibilityType, lastComment.visibilityValue).bind()
+
             reopen().toFailedModuleEither().bind()
         }
     }
 
-    private fun assertCreationIsNotRecent(updated: Long, created: Long) = if ((updated - created) < 2000) {
-        OperationNotNeededModuleResponse.left()
-    } else {
-        Unit.right()
+    private fun assertCreationIsNotRecent(updated: Long, created: Long) = when {
+            (updated - created) < 2000 -> OperationNotNeededModuleResponse.left()
+            else -> Unit.right()
+        }
+
+    private fun assertUpdateWasNotCausedByEditingComment(updated: Long, commentUpdated: Long, commentCreated: Long) = when {
+        updated - commentUpdated >= 2000 -> Unit.right()
+        commentUpdated - commentCreated <= 2000 -> Unit.right()
+        else -> OperationNotNeededModuleResponse.left()
     }
 
-    private fun assertUpdateWasNotCausedByEditingComment(updated: Long, commentUpdated: Long, commentCreated: Long) =
-        if (updated - commentUpdated < 2000 && (commentUpdated - commentCreated) > 2000) {
-            OperationNotNeededModuleResponse.left()
-        } else {
-            Unit.right()
-        }
+    private fun assertCommentWasNotAddedByABlacklistedRole(roles: List<String>?) = when {
+        roles == null -> Unit.right()
+        roles.none { it in blacklistedRoles } -> Unit.right()
+        else -> OperationNotNeededModuleResponse.left()
+    }
+
+    private fun assertCommentIsNotRestrictedToABlacklistedLevel(visibilityType: String?, visibilityValue: String?) = when {
+        visibilityType != "group" -> Unit.right()
+        blacklistedVisibilities.none { it == visibilityValue } -> Unit.right()
+        else -> OperationNotNeededModuleResponse.left()
+    }
 }

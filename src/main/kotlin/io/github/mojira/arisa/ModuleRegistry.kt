4c0faeb5c1eb46caff3768401c00552b942b90e9
SPGoding <SPGoding@outlook.com>
2020-04-23 14:40:08 -0500

    Add ReplaceCommentModule (#153)
    * Add ReplaceCommentModule
    
    * Move the regex out of the config file
    
    * Change requirements from list to map
    
    Co-authored-by: NeunEinser <daniel30797@gmail.com>

--- src/main/kotlin/io/github/mojira/arisa/ModuleRegistry.kt
+++ src/main/kotlin/io/github/mojira/arisa/ModuleRegistry.kt
@@ -45,6 +45,7 @@ import io.github.mojira.arisa.modules.PiracyModule
 import io.github.mojira.arisa.modules.RemoveNonStaffMeqsModule
 import io.github.mojira.arisa.modules.RemoveTriagedMeqsModule
 import io.github.mojira.arisa.modules.ReopenAwaitingModule
+import io.github.mojira.arisa.modules.ReplaceCommentModule
 import io.github.mojira.arisa.modules.ResolveTrashModule
 import io.github.mojira.arisa.modules.RevokeConfirmationModule
 import io.github.mojira.arisa.modules.UpdateLinkedModule
@@ -372,6 +373,24 @@ class ModuleRegistry(jiraClient: JiraClient, private val config: Config) {
                             }
                     },
                 ::reopenIssue.partially1(issue)
+            )
+        }
+
+        register(
+            "ReplaceComment",
+            Modules.ReplaceComment,
+            ReplaceCommentModule()
+        ) { issue, lastRun ->
+            ReplaceCommentModule.Request(
+                lastRun,
+                issue.comments
+                    .map { c ->
+                        ReplaceCommentModule.Comment(
+                            c.updatedDate.toInstant().toEpochMilli(),
+                            c.body,
+                            ::updateCommentBody.partially1(c)
+                        )
+                    }
             )
         }
 

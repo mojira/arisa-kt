915f9fdd7696519b711ab3786db19f8cb8809509
NeunEinser <daniel30797@gmail.com>
2020-04-23 09:02:33 +0200

    Feature/dont reopen after staff edit (#143)
    * Don't reopen awaiting reponse caused by a staff comment or a restricted comment
    
    * ktlint
    
    * fix build
    
    Co-authored-by: Uriel Salischiker <urielsalis@users.noreply.github.com>

--- src/test/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModuleTest.kt
+++ src/test/kotlin/io/github/mojira/arisa/modules/ReopenAwaitingModuleTest.kt
@@ -12,100 +12,163 @@ import io.kotest.matchers.shouldBe
 import java.time.Instant
 
 class ReopenAwaitingModuleTest : StringSpec({
+    val MODULE = ReopenAwaitingModule(
+        listOf("staff", "global-moderators"),
+        listOf("helper", "staff", "global-moderators")
+    )
+
     "should return OperationNotNeededModuleResponse when there is no resolution" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
-        val comment = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request(null, now, updated, listOf(comment)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should return OperationNotNeededModuleResponse when ticket is not in awaiting response" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
-        val comment = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request("Test", now, updated, listOf(comment)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should return OperationNotNeededModuleResponse when ticket is less than 2 seconds old" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(1)
-        val comment = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should return OperationNotNeededModuleResponse when there are no comments" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
         val request = Request("Awaiting Response", now, updated, emptyList()) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should return OperationNotNeededModuleResponse when just the comment was updated" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
-        val comment = Comment(now.plusSeconds(3).toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.plusSeconds(3).toEpochMilli(), now.toEpochMilli(), null, null, null)
         val updated = Instant.now().plusSeconds(3)
         val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when comment is restricted" {
+        val now = Instant.now()
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), "group", "helper", null)
+        val updated = Instant.now().plusSeconds(3)
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when comment author is staff" {
+        val now = Instant.now()
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, listOf("staff"))
+        val updated = Instant.now().plusSeconds(3)
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should grab the last comment" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
-        val commentFail = Comment(now.plusSeconds(3).toEpochMilli(), now.toEpochMilli())
-        val commentSuccess = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val commentFail = Comment(now.plusSeconds(3).toEpochMilli(), now.toEpochMilli(), null, null, null)
+        val commentSuccess = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request("Awaiting Response", now, updated, listOf(commentSuccess, commentFail)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should return FailedModuleResponse with all exceptions when reopening fails" {
-        val module = ReopenAwaitingModule()
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
-        val comment = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request("Awaiting Response", now, updated, listOf(comment)) { RuntimeException().left() }
 
-        val result = module(request)
+        val result = MODULE(request)
 
         result.shouldBeLeft()
         result.a should { it is FailedModuleResponse }
         (result.a as FailedModuleResponse).exceptions.size shouldBe 1
     }
 
-    "should return ModuleResponse when ticket is reopened" {
-        val module = ReopenAwaitingModule()
+    "should reopen when someone answered" {
         val now = Instant.now()
         val updated = Instant.now().plusSeconds(3)
-        val comment = Comment(now.toEpochMilli(), now.toEpochMilli())
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, null)
         val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
 
-        val result = module(request)
+        val result = MODULE(request)
+
+        result.shouldBeRight(ModuleResponse)
+    }
+
+    "should reopen when comment is restricted, but not to a group" {
+        val now = Instant.now()
+        val updated = Instant.now().plusSeconds(3)
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), "not-a-group", "helper", null)
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
+
+        result.shouldBeRight(ModuleResponse)
+    }
+
+    "should reopen when comment is restricted, but not to a blacklisted group" {
+        val now = Instant.now()
+        val updated = Instant.now().plusSeconds(3)
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), "group", "users", null)
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
+
+        result.shouldBeRight(ModuleResponse)
+    }
+
+    "should reopen when comment author has no groups" {
+        val now = Instant.now()
+        val updated = Instant.now().plusSeconds(3)
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, emptyList())
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
+
+        result.shouldBeRight(ModuleResponse)
+    }
+
+    "should reopen when comment author has no blacklisted groups" {
+        val now = Instant.now()
+        val updated = Instant.now().plusSeconds(3)
+        val comment = Comment(now.toEpochMilli(), now.toEpochMilli(), null, null, listOf("Users"))
+        val request = Request("Awaiting Response", now, updated, listOf(comment)) { Unit.right() }
+
+        val result = MODULE(request)
 
         result.shouldBeRight(ModuleResponse)
     }

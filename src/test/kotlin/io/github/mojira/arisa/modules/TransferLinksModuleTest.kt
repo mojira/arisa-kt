e99cafa73a7c821780bd23fbb51db065c2e2f3f6
SPGoding <SPGoding@outlook.com>
2020-05-20 01:51:06 -0500

    Fix TransferLinksModuleTest

--- src/test/kotlin/io/github/mojira/arisa/modules/TransferLinksModuleTest.kt
+++ src/test/kotlin/io/github/mojira/arisa/modules/TransferLinksModuleTest.kt
@@ -1,21 +1,26 @@
 package io.github.mojira.arisa.modules
 
+import arrow.core.Either
+import arrow.core.left
 import arrow.core.right
+import io.github.mojira.arisa.domain.Issue
 import io.github.mojira.arisa.utils.RIGHT_NOW
 import io.github.mojira.arisa.utils.mockIssue
 import io.github.mojira.arisa.utils.mockLink
 import io.github.mojira.arisa.utils.mockLinkedIssue
 import io.kotest.assertions.arrow.either.shouldBeLeft
 import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.assertions.fail
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 /**
  * MC-42 duplicates MC-1.
  */
-private val DUPLICATE_LINK = linkIssues(
+private val DUPLICATES_LINK = linkIssues(
     key1 = "MC-42",
     key2 = "MC-1"
 )
@@ -73,7 +78,7 @@ class TransferLinksModuleTest : StringSpec({
         val module = TransferLinksModule()
         val issue = mockIssue(
             key = "MC-42",
-            links = listOf(DUPLICATE_LINK)
+            links = listOf(DUPLICATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -85,7 +90,7 @@ class TransferLinksModuleTest : StringSpec({
         val module = TransferLinksModule()
         val issue = mockIssue(
             key = "MC-42",
-            links = listOf(DUPLICATE_LINK, RELATES_LINK)
+            links = listOf(DUPLICATES_LINK, RELATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -105,16 +110,17 @@ class TransferLinksModuleTest : StringSpec({
             key2 = "MC-10",
             type = "Relates",
             removeLink = { key1, type, key2 ->
-                linkRemoved=true
+                linkRemoved = true
                 key1 shouldBe "MC-42"
                 type shouldBe "Relates"
                 key2 shouldBe "MC-10"
+                Unit.right()
             }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            links = listOf(DUPLICATE_LINK, linkToTransfer)
+            links = listOf(DUPLICATES_LINK, linkToTransfer)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -134,7 +140,8 @@ class TransferLinksModuleTest : StringSpec({
             key1 = "MC-42",
             key2 = "MC-1",
             removeLink = { _, _, _ ->
-                parentLinkRemoved=true
+                parentLinkRemoved = true
+                Unit.right()
             }
         )
 
@@ -156,7 +163,19 @@ class TransferLinksModuleTest : StringSpec({
         val module = TransferLinksModule()
 
         /**
-         * MC-100 duplicates MC-42.
+         * MC-42 duplicates MC-1.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            createLink = { _, _, _ ->
+                parentLinkAdded = true
+                Unit.right()
+            }
+        )
+
+        /**
+         * MC-42 is duplicated by MC-100.
          */
         val link1 = linkIssues(
             key1 = "MC-42",
@@ -167,11 +186,12 @@ class TransferLinksModuleTest : StringSpec({
                 key1 shouldBe "MC-100"
                 type shouldBe "Duplicate"
                 key2 shouldBe "MC-1"
+                Unit.right()
             }
         )
 
         /**
-         * MC-101 duplicates MC-42.
+         * MC-42 is duplicated by MC-101.
          */
         val link2 = linkIssues(
             key1 = "MC-42",
@@ -182,17 +202,7 @@ class TransferLinksModuleTest : StringSpec({
                 key1 shouldBe "MC-101"
                 type shouldBe "Duplicate"
                 key2 shouldBe "MC-1"
-            }
-        )
-
-        /**
-         * MC-42 duplicates MC-1.
-         */
-        val link = linkIssues(
-            key1 = "MC-42",
-            key2 = "MC-1",
-            createLink = { _, _, _ ->
-                parentLinkAdded = true
+                Unit.right()
             }
         )
 
@@ -212,32 +222,50 @@ class TransferLinksModuleTest : StringSpec({
         var addedToSecondParent = false
         val module = TransferLinksModule()
 
-        val duplicatesLink1 = mockLink(
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                setField = {
-                    addedToFirstParent = true
-                    Unit.right()
+        /**
+         * MC-42 is duplicated by MC-100.
+         */
+        val linkToTransfer = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-100",
+            outwards = false,
+            createLink = { key1, type, key2 ->
+                when (key2) {
+                    "MC-1" -> {
+                        addedToFirstParent = true
+                        key1 shouldBe "MC-100"
+                        type shouldBe "Duplicate"
+                    }
+                    "MC-2" -> {
+                        addedToSecondParent = true
+                        key1 shouldBe "MC-100"
+                        type shouldBe "Duplicate"
+                    }
+                    else -> fail("MC-100 should only duplicate MC-1 and MC-2")
                 }
-            )
+                Unit.right()
+            }
         )
 
-        val duplicatesLink2 = mockLink(
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                setField = {
-                    addedToSecondParent = true
-                    Unit.right()
-                }
-            )
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val duplicatesLink1 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1"
+        )
+
+        /**
+         * MC-42 duplicates MC-2.
+         */
+        val duplicatesLink2 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-2"
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            listOf(duplicatesLink1, duplicatesLink2, RELATES_LINK), listOf(
-                duplicatesLink1, duplicatesLink2,
-                RELATES_LINK
-            )
+            links = listOf(duplicatesLink1, duplicatesLink2, linkToTransfer)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -252,36 +280,52 @@ class TransferLinksModuleTest : StringSpec({
         var secondLinkAdded = false
         val module = TransferLinksModule()
 
-        val outwardsRelates1 = mockLink(
-            type = "Relates",
-            outwards = false,
-            issue = mockLinkedIssue(
-                key = "MC-2",
-                setField = {
-                    firstLinkAdded = true
-                    it.type.shouldBe("Relates")
-                    it.issue.shouldBe("MC-1").right()
-                }
-            )
+        /**
+         * MC-42 relates to MC-10.
+         */
+        val outwardsRelates1 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-10",
+            type = "Relates"
         )
 
-        val outwardsRelates2 = mockLink(
-            type = "Relates",
-            outwards = false,
-            issue = mockLinkedIssue(
-                key = "MC-3",
-                setField = {
-                    secondLinkAdded = true
-                    it.type.shouldBe("Relates")
-                    it.issue.shouldBe("MC-1").right()
+        /**
+         * MC-42 relates to MC-11.
+         */
+        val outwardsRelates2 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-11",
+            type = "Relates"
+        )
+
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val duplicatesLink = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            createLink = { key1, type, key2 ->
+                secondLinkAdded = true
+                when (key2) {
+                    "MC-10" -> {
+                        firstLinkAdded = true
+                        key1 shouldBe "MC-1"
+                        type shouldBe "Relates"
+                    }
+                    "MC-11" -> {
+                        secondLinkAdded = true
+                        key1 shouldBe "MC-1"
+                        type shouldBe "Relates"
+                    }
+                    else -> fail("The parent should only be linked to MC-10 and MC-11")
                 }
-            )
+                Unit.right()
+            }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            listOf(DUPLICATES_LINK, outwardsRelates1, outwardsRelates2),
-            listOf(DUPLICATES_LINK, outwardsRelates1, outwardsRelates2)
+            links = listOf(duplicatesLink, outwardsRelates1, outwardsRelates2)
         )
         val result = module(issue, RIGHT_NOW)
 
@@ -295,31 +339,48 @@ class TransferLinksModuleTest : StringSpec({
         var addedToSecondParent = false
         val module = TransferLinksModule()
 
-        val outwardsRelates = mockLink(
-            type = "Relates",
-            outwards = false,
-            issue = mockLinkedIssue(
-                key = "MC-2",
-                setField = { l ->
-                    when (l.issue) {
-                        "MC-1" -> addedToFirstParent = true
-                        "MC-2" -> addedToSecondParent = true
-                    }
-                    Unit.right()
-                }
-            )
+        /**
+         * MC-42 relates to MC-10.
+         */
+        val outwardsRelates = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-10",
+            type = "Relates"
         )
 
-        val duplicatesLink2 = mockLink(
-            key = "MC-42",
-            issue = mockLinkedIssue(
-                key = "MC-2"
-            )
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val duplicatesLink1 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            createLink = { key1, type, key2 ->
+                addedToFirstParent = true
+                key1 shouldBe "MC-1"
+                type shouldBe "Relates"
+                key2 shouldBe "MC-10"
+                Unit.right()
+            }
+        )
+
+        /**
+         * MC-42 duplicates MC-2.
+         */
+        val duplicatesLink2 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-2",
+            createLink = { key1, type, key2 ->
+                addedToSecondParent = true
+                key1 shouldBe "MC-2"
+                type shouldBe "Relates"
+                key2 shouldBe "MC-10"
+                Unit.right()
+            }
         )
 
         val issue = mockIssue(
-            listOf(DUPLICATES_LINK, duplicatesLink2, outwardsRelates),
-            listOf(DUPLICATES_LINK, duplicatesLink2, outwardsRelates)
+            key = "MC-42",
+            links = listOf(duplicatesLink1, duplicatesLink2, outwardsRelates)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -332,18 +393,21 @@ class TransferLinksModuleTest : StringSpec({
     "should return FailedModuleResponse when removing a link fails" {
         val module = TransferLinksModule()
 
-        val link = mockLink(
+        /**
+         * MC-42 relates to MC-10.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-10",
             type = "Relates",
-            issue = mockLinkedIssue(
-                key = "MC-2"
-            ),
-            remove = { RuntimeException().left() }
+            removeLink = { _, _, _ ->
+                RuntimeException().left()
+            }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            listOf(DUPLICATES_LINK, link),
-            listOf(DUPLICATES_LINK, link)
+            links = listOf(DUPLICATES_LINK, link)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -356,26 +420,33 @@ class TransferLinksModuleTest : StringSpec({
     "should return FailedModuleResponse with all errors when removing multiple links fails" {
         val module = TransferLinksModule()
 
-        val link1 = mockLink(
+        /**
+         * MC-42 relates to MC-10.
+         */
+        val link1 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-10",
             type = "Relates",
-            issue = mockLinkedIssue(
-                key = "MC-2"
-            ),
-            remove = { RuntimeException().left() }
+            removeLink = { _, _, _ ->
+                RuntimeException().left()
+            }
         )
 
-        val link2 = mockLink(
-            key = "MC-42",
+        /**
+         * MC-42 relates to MC-11.
+         */
+        val link2 = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-11",
             type = "Relates",
-            issue = mockLinkedIssue(
-                key = "MC-2"
-            ),
-            remove = { RuntimeException().left() }
+            removeLink = { _, _, _ ->
+                RuntimeException().left()
+            }
         )
 
         val issue = mockIssue(
-            listOf(DUPLICATES_LINK, link1, link2),
-            listOf(DUPLICATES_LINK, link1, link2)
+            key = "MC-42",
+            links = listOf(DUPLICATES_LINK, link1, link2)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -388,16 +459,20 @@ class TransferLinksModuleTest : StringSpec({
     "should return FailedModuleResponse when adding a link fails" {
         val module = TransferLinksModule()
 
-        val link = mockLink(
-            key = "MC-42",
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                setField = { RuntimeException().left() }
-            )
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            createLink = { _, _, _ ->
+                RuntimeException().left()
+            }
         )
+
         val issue = mockIssue(
-            listOf(link, RELATES_LINK),
-            listOf(link, RELATES_LINK)
+            key = "MC-42",
+            links = listOf(link, RELATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -410,18 +485,20 @@ class TransferLinksModuleTest : StringSpec({
     "should return FailedModuleResponse with all errors when adding multiple links fails" {
         val module = TransferLinksModule()
 
-        val link = mockLink(
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                setField = { RuntimeException().left() }
-            )
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            createLink = { _, _, _ ->
+                RuntimeException().left()
+            }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            key = "MC-1",
-            listOf(link, RELATES_LINK, RELATES_LINK),
-            listOf(link, RELATES_LINK, RELATES_LINK)
+            links = listOf(link, RELATES_LINK, RELATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -433,17 +510,21 @@ class TransferLinksModuleTest : StringSpec({
 
     "should return FailedModuleResponse when getting an issue fails" {
         val module = TransferLinksModule()
-        val link = mockLink(
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                getField = { RuntimeException().left() }
-            )
+
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            getFullIssue = {
+                RuntimeException().left()
+            }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            listOf(link, RELATES_LINK),
-            listOf(link, RELATES_LINK)
+            links = listOf(link, RELATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -456,17 +537,20 @@ class TransferLinksModuleTest : StringSpec({
     "should return FailedModuleResponse with all errors when getting an issue fails" {
         val module = TransferLinksModule()
 
-        val link = mockLink(
-            issue = mockLinkedIssue(
-                key = "MC-1",
-                getField = { RuntimeException().left() }
-            )
+        /**
+         * MC-42 duplicates MC-1.
+         */
+        val link = linkIssues(
+            key1 = "MC-42",
+            key2 = "MC-1",
+            getFullIssue = {
+                RuntimeException().left()
+            }
         )
 
         val issue = mockIssue(
             key = "MC-42",
-            listOf(link, link, RELATES_LINK),
-            listOf(link, link, RELATES_LINK)
+            links = listOf(link, link, RELATES_LINK)
         )
 
         val result = module(issue, RIGHT_NOW)
@@ -482,31 +566,32 @@ private fun linkIssues(
     key2: String,
     type: String = "Duplicate",
     outwards: Boolean = true,
-    createLink: (key1: String, type: String, key2: String) -> Unit = { _, _, _ -> Unit },
-    removeLink: (key1: String, key2: String, type: String) -> Unit = { _, _, _ -> Unit }
+    createLink: (key1: String, type: String, key2: String) -> Either<Throwable, Unit> = { _, _, _ -> Unit.right() },
+    removeLink: (key1: String, key2: String, type: String) -> Either<Throwable, Unit> = { _, _, _ -> Unit.right() },
+    getFullIssue: () -> Either<Throwable, Issue> = {
+        mockIssue(
+            key = key2,
+            links = listOf(
+                mockLink(
+                    type = type,
+                    outwards = !outwards,
+                    issue = mockLinkedIssue(
+                        key = key1,
+                        createLink = { linkType, key -> createLink(key1, linkType, key) }
+                    ),
+                    remove = { removeLink(key2, type, key1) }
+                )
+            ),
+            createLink = { linkType, key -> createLink(key2, linkType, key) }
+        ).right()
+    }
 ) = mockLink(
     type = type,
     outwards = outwards,
     issue = mockLinkedIssue(
         key = key2,
-        getFullIssue = {
-            mockIssue(
-                key = key2,
-                links = listOf(
-                    mockLink(
-                        type = type,
-                        outwards = !outwards,
-                        issue = mockLinkedIssue(
-                            key = key1,
-                            createLink = { linkType, key -> createLink(key1, linkType, key).right() }
-                        ),
-                        remove = { removeLink(key2, type, key1).right() }
-                    )
-                ),
-                createLink = { linkType, key -> createLink(key2, linkType, key).right() }
-            ).right()
-        },
-        createLink = { linkType, key -> createLink(key2, linkType, key).right() }
+        getFullIssue = getFullIssue,
+        createLink = { linkType, key -> createLink(key2, linkType, key) }
     ),
-    remove = { removeLink(key1, type, key2).right() }
+    remove = { removeLink(key1, type, key2) }
 )

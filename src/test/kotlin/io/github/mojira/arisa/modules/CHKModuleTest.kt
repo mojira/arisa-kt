357e44a22758d9c5f09e2fb9bb1dd5803d097650
SPGoding <SPGoding@outlook.com>
2020-05-19 21:43:25 -0500

    Fix CHKModuleTest

--- src/test/kotlin/io/github/mojira/arisa/modules/CHKModuleTest.kt
+++ src/test/kotlin/io/github/mojira/arisa/modules/CHKModuleTest.kt
@@ -1,65 +1,79 @@
 package io.github.mojira.arisa.modules
 
 import arrow.core.left
-import arrow.core.right
-import io.github.mojira.arisa.modules.CHKModule.Request
+import io.github.mojira.arisa.utils.getIssue
 import io.kotest.assertions.arrow.either.shouldBeLeft
 import io.kotest.assertions.arrow.either.shouldBeRight
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import java.time.Instant
+
+private val NOW = Instant.now()
 
 class CHKModuleTest : StringSpec({
     "should not process tickets without a confirmation status" {
         val module = CHKModule()
-        val request = Request(null, null) { Unit.right() }
+        val issue = getIssue()
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should not process tickets with a Undefined confirmation status" {
         val module = CHKModule()
-        val request = Request(null, "Undefined") { Unit.right() }
+        val issue = getIssue(
+            confirmationStatus = "Undefined"
+        )
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should not process tickets with a Unconfirmed confirmation status" {
         val module = CHKModule()
-        val request = Request(null, "Unconfirmed") { Unit.right() }
+        val issue = getIssue(
+            confirmationStatus = "Unconfirmed"
+        )
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should not process tickets with CHK already set" {
         val module = CHKModule()
-        val request = Request("chkField", "Confirmed") { Unit.right() }
+        val issue = getIssue(
+            chkField = "chkField",
+            confirmationStatus = "Confirmed"
+        )
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeLeft(OperationNotNeededModuleResponse)
     }
 
     "should process confirmed tickets" {
         val module = CHKModule()
-        val request = Request(null, "Confirmed") { Unit.right() }
+        val issue = getIssue(
+            confirmationStatus = "Confirmed"
+        )
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeRight(ModuleResponse)
     }
 
     "should return FailedModuleResponse when update fails" {
         val module = CHKModule()
-        val request = Request(null, "Confirmed") { RuntimeException().left() }
+        val issue = getIssue(
+            confirmationStatus = "Confirmed",
+            updateCHK = { RuntimeException().left() }
+        )
 
-        val result = module(request)
+        val result = module(issue, NOW)
 
         result.shouldBeLeft()
         result.a should { it is FailedModuleResponse }

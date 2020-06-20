639d2ba744a594a4a8f134e3bfa792a140974ada
SPGoding <SPGoding@outlook.com>
2020-06-20 11:38:25 -0500

    HAVING FUN

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/FixedCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/FixedCommand.kt
@@ -6,7 +6,6 @@ import io.github.mojira.arisa.domain.Issue
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
 import io.github.mojira.arisa.modules.assertTrue
-import io.github.mojira.arisa.modules.toFailedModuleEither
 
 class FixedCommand : Command {
     override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {

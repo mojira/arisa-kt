9a7dffb831630457e6fe7f73bea3d1872d3bcfb5
NeunEinser <daniel30797@gmail.com>
2020-07-01 22:29:42 +0200

    fix crash when user name is null somehow (#349)

--- src/main/kotlin/io/github/mojira/arisa/domain/User.kt
+++ src/main/kotlin/io/github/mojira/arisa/domain/User.kt
@@ -1,7 +1,7 @@
 package io.github.mojira.arisa.domain
 
 data class User(
-    val name: String,
-    val displayName: String,
+    val name: String?,
+    val displayName: String?,
     val getGroups: () -> List<String>?
 )

1f3bfb63d7db384d9c61b761c8bd7d310d9edf8d
SPGoding <SPGoding@outlook.com>
2020-07-30 10:28:51 -0500

    Add ARISA_PURGE_ATTACHMENT command (#393)
    * Add ARISA_PURGE_ATTACHMENT command
    
    * Remove magic number
    
    * dfhjkhg
    
    * Fix CommandModuleTest
    
    * Change the log level of spaming comment btw
    
    * Enable CommandModule on all resolutions
    
    Co-authored-by: Uriel Salischiker <uriel@urielsalis.me>

--- src/main/kotlin/io/github/mojira/arisa/domain/Attachment.kt
+++ src/main/kotlin/io/github/mojira/arisa/domain/Attachment.kt
@@ -3,6 +3,7 @@ package io.github.mojira.arisa.domain
 import java.time.Instant
 
 data class Attachment(
+    val id: String,
     val name: String,
     val created: Instant,
     val mimeType: String,

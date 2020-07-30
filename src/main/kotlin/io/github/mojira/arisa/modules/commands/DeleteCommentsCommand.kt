96987d91b7689a498f16d831a32d35ba49529ef8
urielsalis <uriel@urielsalis.me>
2020-07-30 16:16:06 +0200

    Sleep between restricts

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/DeleteCommentsCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/DeleteCommentsCommand.kt
@@ -7,15 +7,20 @@ import io.github.mojira.arisa.domain.Issue
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
 import io.github.mojira.arisa.modules.assertTrue
+import java.util.concurrent.TimeUnit
 
 class DeleteCommentsCommand : Command {
     override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
         assertTrue(arguments.size > 1).bind()
         val name = arguments.asList().subList(1, arguments.size).joinToString(" ")
         val comments = issue.comments
-        comments.filter { it.visibilityValue != "staff" }.filter { it.author.name == name }.forEach {
-            it.restrict("Removed by arisa")
-        }
+        comments.filter { it.visibilityValue != "staff" }.filter { it.author.name == name }
+            .forEachIndexed { index, it ->
+                it.restrict("Removed by arisa")
+                if (index % 10 == 0) {
+                    TimeUnit.SECONDS.sleep(1)
+                }
+            }
         ModuleResponse.right()
     }
 }

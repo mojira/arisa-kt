a897e461b5a5038099fe03ebc38f1d374b2e41cb
Uriel Salischiker <uriel@urielsalis.me>
2020-07-30 15:32:52 +0200

    Delete comments command (#396)

---
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/DeleteCommentsCommand.kt
@@ -0,0 +1,19 @@
+package io.github.mojira.arisa.modules.commands
+
+import arrow.core.Either
+import arrow.core.extensions.fx
+import io.github.mojira.arisa.domain.Issue
+import io.github.mojira.arisa.modules.ModuleError
+import io.github.mojira.arisa.modules.ModuleResponse
+import io.github.mojira.arisa.modules.assertTrue
+
+class DeleteCommentsCommand : Command {
+    override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
+        assertTrue(arguments.size > 1).bind()
+        val name = arguments.asList().subList(1, arguments.size).joinToString(" ")
+        val comments = issue.comments
+        comments.filter { it.author.name == name }.forEach {
+            it.restrict("Removed by arisa")
+        }
+    }
+}

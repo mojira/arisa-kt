587f3134b72cbef9dd841895daf3dafab5c40a0f
Uriel Salischiker <uriel@urielsalis.me>
2020-07-30 16:02:17 +0200

    Add restriction (#397)
    * Filter out already restricted commands
    
    * Filter out already restricted commands

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/DeleteCommentsCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/DeleteCommentsCommand.kt
@@ -2,6 +2,7 @@ package io.github.mojira.arisa.modules.commands
 
 import arrow.core.Either
 import arrow.core.extensions.fx
+import arrow.core.right
 import io.github.mojira.arisa.domain.Issue
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
@@ -12,8 +13,9 @@ class DeleteCommentsCommand : Command {
         assertTrue(arguments.size > 1).bind()
         val name = arguments.asList().subList(1, arguments.size).joinToString(" ")
         val comments = issue.comments
-        comments.filter { it.author.name == name }.forEach {
+        comments.filter { it.visibilityValue != "staff" }.filter { it.author.name == name }.forEach {
             it.restrict("Removed by arisa")
         }
+        ModuleResponse.right()
     }
 }

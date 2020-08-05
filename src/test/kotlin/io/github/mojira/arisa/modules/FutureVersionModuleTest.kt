7b432177188179a032790bf8884428e200174fd1
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-05 14:27:37 -0500

    Fixed Test

--- src/test/kotlin/io/github/mojira/arisa/modules/FutureVersionModuleTest.kt
+++ src/test/kotlin/io/github/mojira/arisa/modules/FutureVersionModuleTest.kt
@@ -249,34 +249,19 @@ class FutureVersionModuleTest : StringSpec({
         isResolved.shouldBeTrue()
     }
 
-    "should remove future versions added upon ticket creation by users" {
-        val module = FutureVersionModule("message", "panel")
-        val issue = mockIssue(
-            reporter = mockUser(
-                getGroups = { listOf("user") }
-            ),
-            affectedVersions = listOf(FUTURE_VERSION),
-            project = mockProject(
-                versions = listOf(RELEASED_VERSION)
-            )
-        )
-
-        val result = module(issue, TWO_SECONDS_AGO)
-
-        result.shouldBeRight(ModuleResponse)
-    }
-    
-    "should not resolve tickets if already resolved" {
+    "should not resolve if already resolved" {
         var preResolved = true
 
         val module = FutureVersionModule("message", "panel")
         val issue = mockIssue(
-                affectedVersions = listOf(FUTURE_VERSION),
-                project = mockProject(
-                        versions = listOf(RELEASED_VERSION)
-                ),
-                resolution = "Invalid",
-                resolveAsAwaitingResponse = { preResolved = false }
+            created = FIVE_SECONDS_AGO,
+            resolution = "Invalid",
+            affectedVersions = listOf(FUTURE_VERSION, RELEASED_VERSION),
+            changeLog = listOf(ADD_FUTURE_VERSION),
+            project = mockProject(
+                versions = listOf(RELEASED_VERSION)
+            ),
+            resolveAsAwaitingResponse = { preResolved = false }
         )
 
         val result = module(issue, TWO_SECONDS_AGO)

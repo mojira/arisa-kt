6f75c89d9ebdc3286c86c056f8704c755455e9d5
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-18 18:32:36 -0500

    Update FixedCommand.kt

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/FixedCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/FixedCommand.kt
@@ -3,7 +3,6 @@ package io.github.mojira.arisa.modules.commands
 import arrow.core.Either
 import arrow.core.extensions.fx
 import io.github.mojira.arisa.domain.Issue
-import io.github.mojira.arisa.modules.CommandModule
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
 import io.github.mojira.arisa.modules.assertTrue
@@ -12,7 +11,6 @@ class FixedCommand : Command {
     override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
         assertTrue(arguments.size > 1).bind()
         val version = arguments.asList().subList(1, arguments.size).joinToString(" ")
-        CommandModule().modRestricted = true
         issue.markAsFixedWithSpecificVersion(version)
     }
 }

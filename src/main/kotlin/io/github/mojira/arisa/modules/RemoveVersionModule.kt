e675763ef5161258616638e305215ad9899e6214
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-29 10:26:09 -0500

    Add unresolved check

--- src/main/kotlin/io/github/mojira/arisa/modules/RemoveVersionModule.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/RemoveVersionModule.kt
@@ -19,7 +19,7 @@ class RemoveVersionModule : Module {
 
     private fun Issue.getExtraVersionsLatelyAddedByNonVolunteers(lastRun: Instant): List<String> =
         if (created.isAfter(lastRun)) {
-            if (isVolunteer(reporter?.getGroups?.invoke())) {
+            if (isVolunteer(reporter?.getGroups?.invoke()) || resolution == "Unresolved") {
                 emptyList()
             } else {
                 affectedVersions.map { ver -> ver.id }

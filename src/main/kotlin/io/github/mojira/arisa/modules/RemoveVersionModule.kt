417ef91fbecf0bf35d251fbc2d17f879216d65bc
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-29 14:26:47 -0500

    Remove extra added versions to resolved tickets by users (#422)
    * Add original file
    
    * Add test file
    
    * Update arisa.json
    
    * register modules
    
    * Create Config spec
    
    * Remove extra if statement
    
    * Remove empty
    
    * Add locale
    
    * Remove whitespace
    
    * Whitespace
    
    * Fix locale
    
    * Update RemoveVersionModule.kt
    
    * Remove locale
    
    * Unused imports
    
    * Remove extra tests
    
    * Change group definition
    
    Co-authored-by: NeunEinser <daniel30797@gmail.com>
    
    * Change version id
    
    * Add unresolved check
    
    * Max line length fix
    
    * whitespace is the bane of my existence
    
    * Move resolution check?
    
    * Move resolution check again
    
    * Add filter for resolved tickets?
    
    * Change resolution check
    
    * Update RemoveVersionModule.kt
    
    * Remove check
    
    * Update RemoveVersionModuleTest.kt
    
    * Update RemoveVersionModule.kt
    
    Co-authored-by: NeunEinser <daniel30797@gmail.com>

---
+++ src/main/kotlin/io/github/mojira/arisa/modules/RemoveVersionModule.kt
@@ -0,0 +1,39 @@
+package io.github.mojira.arisa.modules
+
+import arrow.core.Either
+import arrow.core.extensions.fx
+import io.github.mojira.arisa.domain.Issue
+import java.time.Instant
+
+class RemoveVersionModule : Module {
+    override fun invoke(issue: Issue, lastRun: Instant): Either<ModuleError, ModuleResponse> = with(issue) {
+        Either.fx {
+            val addedVersions = getExtraVersionsLatelyAddedByNonVolunteers(lastRun)
+            val removeAddedVersions = affectedVersions
+                .filter { it.id in addedVersions }
+                .map { it.remove }
+            assertNotEmpty(removeAddedVersions).bind()
+            removeAddedVersions.forEach(::run)
+        }
+    }
+
+    private fun Issue.getExtraVersionsLatelyAddedByNonVolunteers(lastRun: Instant): List<String> =
+        if (created.isAfter(lastRun)) {
+            if (isVolunteer(reporter?.getGroups?.invoke())) {
+                emptyList()
+            } else {
+                affectedVersions.map { ver -> ver.id }
+            }
+        } else {
+            changeLog
+                .asSequence()
+                .filter { it.created.isAfter(lastRun) }
+                .filter { it.field.toLowerCase() == "version" }
+                .filterNot { isVolunteer(it.getAuthorGroups()) }
+                .mapNotNull { it.changedTo }
+                .toList()
+        }
+
+    private fun isVolunteer(groups: List<String>?) =
+        groups?.any { it == "helper" || it == "global-moderators" || it == "staff" } ?: false
+}

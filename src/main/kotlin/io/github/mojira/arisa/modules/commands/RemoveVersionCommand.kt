5c3321ed11cb9c83f808bc9d6f8ad2fca325e146
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-09-04 10:34:00 -0500

    Add import

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/RemoveVersionCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/RemoveVersionCommand.kt
@@ -6,13 +6,13 @@ import io.github.mojira.arisa.domain.Issue
 import io.github.mojira.arisa.modules.ModuleError
 import io.github.mojira.arisa.modules.ModuleResponse
 import io.github.mojira.arisa.modules.assertTrue
+import io.github.mojira.arisa.modules.assertNotNull
 
 class RemoveVersionCommand : Command {
     override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
         assertTrue(arguments.size > 1).bind()
         val version = arguments.asList().subList(1, arguments.size).joinToString(" ")
         assertTrue(issue.affectedVersions.any { it.name == version }).bind()
-        assertTrue(issue.project.versions.any { it.name == version }).bind()
         assertNotNull(issue.affectedVersions.indexOf(issue.project.versions.first { it.name == version })).bind()
         val idNum = issue.affectedVersions.indexOf(issue.project.versions.first { it.name == version })
         issue.affectedVersions[idNum].remove

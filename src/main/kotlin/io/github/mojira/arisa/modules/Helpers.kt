51375258019e85cf2a693da32a0565893dc0ef9a
SunCatMC <suncat.game@ya.ru>
2020-09-08 21:30:17 +0300

    initial commit, implemented helper function deleteLink

--- src/main/kotlin/io/github/mojira/arisa/modules/Helpers.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/Helpers.kt
@@ -3,8 +3,10 @@
 package io.github.mojira.arisa.modules
 
 import arrow.core.Either
+import arrow.core.extensions.fx
 import arrow.core.left
 import arrow.core.right
+import io.github.mojira.arisa.domain.Issue
 import java.time.Instant
 
 fun <T> Either<Throwable, T>.toFailedModuleEither() = this.bimap(
@@ -115,4 +117,80 @@ fun assertAfter(instant1: Instant, instant2: Instant) = if (instant1.isAfter(ins
     Unit.right()
 } else {
     OperationNotNeededModuleResponse.left()
+}
+
+fun convertLinks(vararg arguments: String): Array<String> {
+    var newArgList = mutableListOf<String>()
+    for (arg in arguments) {
+        var key = arg
+        key = key.dropLastWhile { it == ',' }
+        if (key.matches(Regex("https://bugs.mojang.com/browse/[A-Z]+\\-[0-9]+"))) {
+            key = key.drop(31)
+        }
+        newArgList.add(newArgList.size, key)
+    }
+    return newArgList.toTypedArray()
+}
+
+data class LinkType(
+    val name: List<String>,
+    val id: String,
+    val outwards: Boolean
+) {
+    lateinit var nameVariants: Set<String>
+    operator fun invoke() {
+        nameVariants = concatenateCombinations(name)
+    }
+}
+
+val linkTypes = listOf<LinkType>(
+    LinkType(listOf("relates", "to"), "Relates", true),
+    LinkType(listOf("duplicates"), "Duplicates", false),
+    LinkType(listOf("is","duplicated", "by"), "Duplicates", true),
+    TODO("Fill the list")
+)
+
+private fun concatenateCombinations(list : List<String>): Set<String> {
+    var newList = mutableListOf<String>()
+    for (word in list) {
+        for (item in newList) {
+            var tmp = mutableListOf<String>()
+            tmp.add("$item $word")
+            newList.addAll(0, tmp)
+        }
+        newList.add(word)
+    }
+    return newList.toSet()
+}
+
+fun addLinks(issue: Issue, type: String, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
+    val tmp = linkTypes.filter {
+        type in it.nameVariants
+    }
+    assertNotNull(tmp)
+    assertTrue(tmp.size == 1)
+    val linkType = tmp[0]
+    for (key in arguments) {
+        assertTrue(key.matches(Regex("[A-Z]+\\-[0-9]+")))
+    }
+    TODO()
+}
+
+fun removeLinks(issue: Issue, type: String, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
+    val tmp = linkTypes.filter {
+        type in it.nameVariants
+    }
+    assertNotNull(tmp)
+    assertTrue(tmp.size == 1)
+    val linkType = tmp[0]
+    for (key in arguments) {
+        assertTrue(key.matches(Regex("[A-Z]+\\-[0-9]+")))
+    }
+    for (key in arguments) {
+        val link = issue.links.find {
+            it.type == linkType.id && it.issue.key == key && (linkType.id == "Relates" || it.outwards == linkType.outwards)
+        }
+        assertNotNull(link)
+        link?.remove?.invoke()
+    }
 }

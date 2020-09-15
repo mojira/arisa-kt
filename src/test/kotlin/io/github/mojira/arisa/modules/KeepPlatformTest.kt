56f8f6bcb5fef20df2ff867fbcaa3f048bdf5f23
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-09-15 09:58:05 -0500

    Create KeepPlatformTest.kt

---
+++ src/test/kotlin/io/github/mojira/arisa/modules/KeepPlatformTest.kt
@@ -0,0 +1,112 @@
+package io.github.mojira.arisa.modules
+
+import arrow.core.right
+import io.github.mojira.arisa.utils.RIGHT_NOW
+import io.github.mojira.arisa.utils.mockChangeLogItem
+import io.github.mojira.arisa.utils.mockComment
+import io.github.mojira.arisa.utils.mockIssue
+import io.kotest.assertions.arrow.either.shouldBeLeft
+import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+private val CHANGE_PLATFORM = mockChangeLogItem(
+    created = RIGHT_NOW.minusSeconds(10),
+    field = "platform",
+    changedFromString = "Amazon"
+)
+
+class KeepPlatformModuleTest : StringSpec({
+    "should return OperationNotNeededModuleResponse when keep platform tag is null" {
+        val module = KeepPlatformModule(null)
+        val comment = mockComment("MEQS_KEEP_PLATFORM", visibilityType = "group", visibilityValue = "staff")
+        val issue = mockIssue(
+            comments = listOf(comment),
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when comments are empty" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when no comment contains platform tag" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "Hello world!",
+            visibilityType = "group",
+            visibilityValue = "staff"
+        )
+        val issue = mockIssue(
+            comments = listOf(comment),
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when the comment isn't restricted to staff group" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM"
+        )
+        val issue = mockIssue(
+            comments = listOf(comment),
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+"should return OperationNotNeededModuleResponse when Ticket’s platform was changed by helper" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM"
+        )
+       val changeLogItem = io.github.mojira.arisa.modules.mockChangeLogItem { listOf("helper") }
+            comments = listOf(comment),
+            val issue = mockIssue(
+            platform = "Xbox One",
+            changeLog = listOf(changeLogItem)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+ "should set back to platform set by volunteer, when regular user changes platform" {
+        var changedPlatform = ""
+
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM"
+        )
+        val volunteerChange = io.github.mojira.arisa.modules.mockChangeLogItem { listOf("staff") }
+        val userChange = mockChangeLogItem(value = "None") { listOf("users") }
+        val issue = mockIssue(
+            comments = listOf(comment),
+            platform = "None",
+            changeLog = listOf(volunteerChange, userChange),
+            updatePlatform = { changedPlatform = it; Unit.right() }
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeRight(ModuleResponse)
+        changedPlatform.shouldBe("Amazon")
+    }

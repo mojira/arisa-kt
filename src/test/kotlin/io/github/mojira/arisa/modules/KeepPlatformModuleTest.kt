7234c0dc7ee54da62cedca6809d47dd83ffc8907
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-09-22 10:39:17 -0500

    Change platforms to multiple if duplicate tickets have a different platform (MEQS_KEEP_PLATFORM to prevent changes) (#432)
    * Update arisa.json
    
    * Update Arisa.kt
    
    * Fix typos
    
    * Update Functions.kt
    
    * Update Mappers.kt
    
    * Update Issue.kt
    
    * Typo
    
    * Typo
    
    * Update MockDomain.kt
    
    * Update ModuleRegistry.kt
    
    * Add the module in
    
    * Add the module test in
    
    * MaxLineLength Fix
    
    * Indentation Fix?
    
    * Fix failing tests
    
    * Update MultiplePlatformsModuleTest.kt
    
    * Update MultiplePlatformsModuleTest.kt
    
    * Update arisa.json
    
    * Update arisa.json
    
    * Add custom field
    
    * Update arisa.json
    
    * Remove unused variables
    
    * Another Typo Fix
    
    * Create KeepPlatform.kt
    
    * Create KeepPlatformTest.kt
    
    * Rename KeepPlatformTest.kt to KeepPlatformModuleTest.kt
    
    * Rename KeepPlatform.kt to KeepPlatformModule.kt
    
    * Update arisa.json
    
    * Update ModuleRegistry.kt
    
    * Update Arisa.kt
    
    * Update ModuleRegistry.kt
    
    * Update Arisa.kt
    
    * Missing Imports
    
    * Typo
    
    * Fix more formatting errors
    
    * Update KeepPlatformModuleTest.kt
    
    * Add missing parenthesis
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * changelog items are private?
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Why isn't the module giving a response :thinking:
    
    * Add moved timeframe
    
    * changedToString?
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Add mock change log item
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Update KeepPlatformModule.kt
    
    * Update KeepPlatformModule.kt
    
    * Whitespace
    
    * Wrong change log item
    
    * Update KeepPlatformModuleTest.kt
    
    * match volunteer to current platform
    
    * Update KeepPlatformModule.kt
    
    * Update KeepPlatformModuleTest.kt
    
    * Remove unused import
    
    * Update KeepPlatformModuleTest.kt
    
    * Finish the pr
    
    * Update ModuleRegistry.kt
    
    * Update MultiplePlatformsModule.kt
    
    * Update MultiplePlatformsModuleTest.kt
    
    * Remove extra argument
    
    * Remove Either.fx
    
    * Revert
    
    * Max Line Length
    
    * Remove lists from strings on Arisa.json
    
    * Remove last list from string
    
    Co-authored-by: Uriel Salischiker <uriel@urielsalis.me>

---
+++ src/test/kotlin/io/github/mojira/arisa/modules/KeepPlatformModuleTest.kt
@@ -0,0 +1,118 @@
+package io.github.mojira.arisa.modules
+
+import arrow.core.right
+import io.github.mojira.arisa.utils.RIGHT_NOW
+import io.github.mojira.arisa.utils.mockChangeLogItem
+import io.github.mojira.arisa.utils.mockComment
+import io.github.mojira.arisa.utils.mockIssue
+import io.kotest.assertions.arrow.either.shouldBeLeft
+import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.time.Instant
+
+private val CHANGE_PLATFORM = mockChangeLogItem(
+    created = RIGHT_NOW.minusSeconds(10),
+    field = "platform",
+    changedFromString = "Amazon"
+)
+
+class KeepPlatformModuleTest : StringSpec({
+    "should return OperationNotNeededModuleResponse when comments are empty" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when no comment contains platform tag" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "Hello world!",
+            visibilityType = "group",
+            visibilityValue = "staff"
+        )
+        val issue = mockIssue(
+            comments = listOf(comment),
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when the comment isn't restricted to staff group" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM"
+        )
+        val issue = mockIssue(
+            comments = listOf(comment),
+            changeLog = listOf(CHANGE_PLATFORM)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when Ticket’s platform was changed by staff" {
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM",
+            visibilityType = "group",
+            visibilityValue = "staff"
+        )
+        val changeLogItem = mockPlatformChangeLogItem { listOf("staff") }
+        val issue = mockIssue(
+            comments = listOf(comment),
+            platform = "Amazon",
+            changeLog = listOf(changeLogItem)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should set back to platform set by volunteer, when regular user changes platform" {
+        var changedPlatform = ""
+
+        val module = KeepPlatformModule("MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+            body = "MEQS_KEEP_PLATFORM",
+            visibilityType = "group",
+            visibilityValue = "staff"
+        )
+        val volunteerChange = mockPlatformChangeLogItem { listOf("staff") }
+        val userChange = mockPlatformChangeLogItem(value = "None") { listOf("users") }
+        val issue = mockIssue(
+            comments = listOf(comment),
+            platform = "None",
+            changeLog = listOf(volunteerChange, userChange),
+            updatePlatforms = { changedPlatform = it; Unit.right() }
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeRight(ModuleResponse)
+        changedPlatform.shouldBe("Amazon")
+    }
+})
+
+private fun mockPlatformChangeLogItem(
+    created: Instant = RIGHT_NOW,
+    field: String = "Platform",
+    value: String = "Amazon",
+    getAuthorGroups: () -> List<String>? = { emptyList() }
+) = mockChangeLogItem(
+    created = created,
+    field = field,
+    changedToString = value,
+    getAuthorGroups = getAuthorGroups
+)
---
+++ src/test/kotlin/io/github/mojira/arisa/modules/MultiplePlatformsModuleTest.kt
@@ -0,0 +1,164 @@
+package io.github.mojira.arisa.modules
+
+import arrow.core.right
+import io.github.mojira.arisa.utils.RIGHT_NOW
+import io.github.mojira.arisa.utils.mockIssue
+import io.github.mojira.arisa.utils.mockLink
+import io.github.mojira.arisa.utils.mockLinkedIssue
+import io.github.mojira.arisa.utils.mockComment
+import io.kotest.assertions.arrow.either.shouldBeLeft
+import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+private val duplicatedLink = mockLink(
+    outwards = false,
+    issue = mockLinkedIssue(
+        getFullIssue = { mockIssue(platform = "None").right() }
+    )
+)
+private val duplicatedLink2 = mockLink(
+    outwards = false,
+    issue = mockLinkedIssue(
+        getFullIssue = { mockIssue(platform = "Amazon").right() }
+    )
+)
+private val relatesLink = mockLink(
+    type = "Relates"
+)
+private val duplicatesLink = mockLink()
+
+class MultiplePlatformsModuleTest : StringSpec({
+    "should return OperationNotNeededModuleResponse when there are no links" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Xbox One",
+            links = emptyList()
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when there are only Relates links" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Xbox One",
+            links = listOf(relatesLink)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when there are only outwards Duplicate links" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Amazon",
+            links = listOf(duplicatesLink)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when Platforms are already set to Multiple" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Multiple",
+            links = listOf(duplicatedLink2)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when Platform is the same as the child report" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Amazon",
+            links = listOf(duplicatedLink2)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when child report Platform is blacklisted" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Xbox One",
+            links = listOf(duplicatedLink)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when the meqs comment is added" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val comment = mockComment(
+                body = "MEQS_KEEP_PLATFORM",
+                visibilityType = "group",
+                visibilityValue = "staff"
+        )
+        val issue = mockIssue(
+                comments = listOf(comment),
+                platform = "Xbox One",
+                links = listOf(duplicatedLink2)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when Platform is null and there is a duplicate" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = null,
+            links = listOf(duplicatedLink2)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should return OperationNotNeededModuleResponse when Platform is empty and there is a duplicate" {
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "",
+            links = listOf(duplicatedLink2)
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeLeft(OperationNotNeededModuleResponse)
+    }
+
+    "should set to Multiple when Platform is Xbox One and there is a duplicate" {
+        var changedPlatform = ""
+
+        val module = MultiplePlatformsModule(listOf("Xbox One", "Amazon"), "Multiple", listOf("None"), "MEQS_KEEP_PLATFORM")
+        val issue = mockIssue(
+            platform = "Xbox One",
+            links = listOf(duplicatedLink2),
+            updatePlatforms = {
+                changedPlatform = it
+                Unit.right()
+            }
+        )
+
+        val result = module(issue, RIGHT_NOW)
+
+        result.shouldBeRight(ModuleResponse)
+        changedPlatform.shouldBe("Multiple")
+    }
+})

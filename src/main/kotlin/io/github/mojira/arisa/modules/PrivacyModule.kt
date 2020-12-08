9763155371e14f77ae8f7fc3b897258adcbcf6bb
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-12-08 10:29:08 -0600

    Update PrivacyModule.kt

--- src/main/kotlin/io/github/mojira/arisa/modules/PrivacyModule.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/PrivacyModule.kt
@@ -3,7 +3,6 @@ package io.github.mojira.arisa.modules
 import arrow.core.Either
 import arrow.core.extensions.fx
 import io.github.mojira.arisa.domain.CommentOptions
-import io.github.mojira.arisa.domain.Comment
 import io.github.mojira.arisa.domain.Issue
 import java.time.Instant
 

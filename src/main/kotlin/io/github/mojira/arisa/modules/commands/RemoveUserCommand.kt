884b825a0b8bdbbd0f954939cecd194fbd085a2a
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-01-29 10:19:04 -0600

    Update RemoveUserCommand.kt

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/RemoveUserCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/RemoveUserCommand.kt
@@ -23,7 +23,10 @@ class RemoveUserCommand : Command {
     val regex = "\"https:\\/\\/bugs\\.mojang\\.com\\/browse\\/(.*)\">".toRegex()
     override fun invoke(issue: Issue, vararg arguments: String): Either<ModuleError, ModuleResponse> = Either.fx {
         assertTrue(arguments.size > 1).bind()
-        val name = URLEncoder.encode(arguments.asList().subList(1, arguments.size).joinToString(" "), StandardCharsets.UTF_8.toString())
+        val name = URLEncoder.encode(
+            arguments.asList().subList(1, arguments.size).joinToString(" "),
+            StandardCharsets.UTF_8.toString()
+        )
         val streamName = name.replace("+", "_").replace("_", "%5C_")
         val request = BasicHttpRequest("GET", "/activity?maxResults=200&streams=user+IS+$streamName")
         credentials.authenticate(request)

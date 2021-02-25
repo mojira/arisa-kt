c11da34b0a895fef62563e716a08e884b6c20978
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-02-25 17:10:45 -0600

    possibly fix max line length

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/FixCapitalizationCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/FixCapitalizationCommand.kt
@@ -5,7 +5,8 @@ import io.github.mojira.arisa.domain.Issue
 class FixCapitalizationCommand : Command1<String> {
     override operator fun invoke(issue: Issue, arg: String): Int {
         val capitalizationRegex =
-                """(?<=\.\s|^|!\s|\?\s)[A-Z][A-Za-z0-9]*((\s|,\s|;\s|:\s)[A-Z][A-Za-z-'0-9]*)*(?=\.|${'$'}|!|\?)""".toRegex()
+                """(?<=\.\s|^|!\s|\?\s)[A-Z][A-Za-z0-9]*((\s|,\s|;\s|:\s)[A-Z][A-Za-z-'0-9]*)*(?=\.|${'$'}|!|\?)"""
+                    .toRegex()
 
         var newDescription = issue.description!!
         val matchesDescription = capitalizationRegex.findAll(newDescription)

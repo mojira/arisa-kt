e6bda389bc3dd038612cb268e55ec50c0e78f163
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-02-28 13:49:57 -0600

    Allow capitalization exceptions in the FixCapitalizationCommand

--- src/main/kotlin/io/github/mojira/arisa/modules/commands/FixCapitalizationCommand.kt
+++ src/main/kotlin/io/github/mojira/arisa/modules/commands/FixCapitalizationCommand.kt
@@ -9,12 +9,22 @@ class FixCapitalizationCommand : Command1<String> {
                 """(?<=\.\s|^|!\s|\?\s|\n)[A-Z][A-Za-z-'0-9]*((\s|,\s|;\s|:\s)[A-Z][A-Za-z-'0-9]*)*(?=\.|${'$'}|!|\?|\n)"""
                     .toRegex()
 
+        val exceptions = listOf(
+            ' i ',
+            ' minecraft ',
+            ' mojang 
+        )
+
         var newDescription = issue.description!!
         val matchesDescription = capitalizationRegex.findAll(newDescription)
         matchesDescription
                 .map { it.groupValues[0] }
                 .forEach {
                     newDescription = newDescription.replace(it, it.toLowerCase().capitalize())
+                }
+        exceptions
+                .forEach {
+                    newDesctiption = newDescription.replace(it, it.capitalize())
                 }
         if (newDescription == issue.description) {
             throw CommandExceptions.NO_CAPITALIZATION_MATCHES.create(arg)

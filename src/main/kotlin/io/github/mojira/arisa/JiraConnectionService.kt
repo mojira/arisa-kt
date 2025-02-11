910ff270910478a2dd2cbb27e132510f1b02996e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2025-02-11 16:44:08 +0100

    fix: return new JiraClient

--- src/main/kotlin/io/github/mojira/arisa/JiraConnectionService.kt
+++ src/main/kotlin/io/github/mojira/arisa/JiraConnectionService.kt
@@ -2,7 +2,6 @@ package io.github.mojira.arisa
 
 import com.uchuhimo.konf.Config
 import io.github.mojira.arisa.infrastructure.config.Arisa
-import io.github.mojira.arisa.infrastructure.jira.IncorrectlyCapitalizedUsernameException
 import io.github.mojira.arisa.infrastructure.jira.connectToJira
 import java.lang.Long.max
 import java.time.Duration
@@ -41,8 +40,8 @@ class JiraConnectionService(
     private fun establishConnection(): Exception? {
         return try {
             val client = connectToJira(
-                config[Arisa.Credentials.username],
-                config[Arisa.Credentials.password],
+                config[Arisa.Credentials.email],
+                config[Arisa.Credentials.apiToken],
                 config[Arisa.Issues.url]
             )
 
@@ -108,13 +107,8 @@ class JiraConnectionService(
     fun connect() {
         val exception = establishConnection() ?: return
 
-        // Rethrow; startup should fail and Arisa config should be fixed
-        if (exception is IncorrectlyCapitalizedUsernameException) {
-            throw exception
-        } else {
-            log.error("Could not connect to Jira", exception)
-            reconnect()
-        }
+        log.error("Could not connect to Jira", exception)
+        reconnect()
     }
 
     /**

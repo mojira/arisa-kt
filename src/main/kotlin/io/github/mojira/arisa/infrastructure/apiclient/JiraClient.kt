4e2d1bf703f9c09b0af2b440662945b1859fee40
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2025-02-12 16:35:10 +0100

    feat: add retrofit

--- src/main/kotlin/io/github/mojira/arisa/infrastructure/apiclient/JiraClient.kt
+++ src/main/kotlin/io/github/mojira/arisa/infrastructure/apiclient/JiraClient.kt
@@ -1,14 +1,16 @@
 package io.github.mojira.arisa.infrastructure.apiclient
 
+import io.github.mojira.arisa.infrastructure.apiclient.models.IssueBean
 import io.github.mojira.arisa.infrastructure.apiclient.models.Project
 import io.github.mojira.arisa.infrastructure.apiclient.models.SearchResults
 import kotlinx.serialization.json.Json
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.RequestBody.Companion.toRequestBody
 import okio.IOException
-
-private val JSON = "application/json".toMediaType()
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.http.*
 
 /**
  * Adds authentication headers to the request.
@@ -17,7 +19,7 @@ class BasicAuthInterceptor(
     private val email: String,
     private val apiToken: String
 ) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         val originalRequest: Request = chain.request()
 
         val credentials = Credentials.basic(email, apiToken)
@@ -29,78 +31,93 @@ class BasicAuthInterceptor(
     }
 }
 
+interface JiraApi {
+    @GET("project/{key}")
+    fun getProject(
+        @Path("key") key: String
+    ): Call<Project>
+
+    @GET("issue/{key}")
+    fun getIssue(
+        @Path("key") key: String,
+        @Query("fields") fields: String = "*all",
+        @Query("expand") expand: String = "changelog"
+    ): Call<IssueBean>
+
+    @POST("search")
+    fun searchIssues(
+        @Body request: JiraSearchRequest
+    ): Call<SearchResults>
+}
+
 class JiraClient(
     private val jiraUrl: String,
     private val email: String,
     private val apiToken: String
 ) {
-    private final val API_ENDPOINT = jiraUrl.plus("rest/api/3")
-    private val httpClient: OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(BasicAuthInterceptor(email, apiToken))
-        .build()
-    private val JsonWithUnknownKeys = Json{ignoreUnknownKeys = true}
+    private val jiraApi: JiraApi
 
-    private fun constructURL(url: String): String {
-        return this.API_ENDPOINT.plus(url)
-    }
-
-    private fun getRequest(url: String): Response {
-        val request = Request.Builder()
-            .url(constructURL(url))
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Accept", "application/json")
+    init {
+        val httpClient: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(BasicAuthInterceptor(email, apiToken))
             .build()
 
-        return this.httpClient.newCall(request).execute()
-    }
+        val apiBaseUrl = jiraUrl.plus("rest/api/3/")
+        val mediaType = "application/json".toMediaType()
+        val json = Json { ignoreUnknownKeys = true }
 
-    private fun postRequest(url: String, payload: RequestBody): Response {
-        val request  = Request.Builder()
-            .url(constructURL(url))
-            .post(payload)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Accept", "application/json")
+        val retrofit = Retrofit.Builder()
+            .baseUrl(apiBaseUrl)
+            .client(httpClient)
+            .addConverterFactory(json.asConverterFactory(mediaType))
             .build()
 
-        return this.httpClient.newCall(request).execute()
+        jiraApi = retrofit.create(JiraApi::class.java)
     }
 
-     fun searchIssues(
-         jql: String,
-         fields: List<String>,
-         expand: List<String> = emptyList(),
-         maxResults: Int,
-         startAt: Int = 0,
-     ): SearchResults {
-         val payload = Json.encodeToString(JiraSearchRequest(
-             expand = expand,
-             fields = fields,
-             jql = jql,
-             maxResults = maxResults,
-             startAt = startAt
-         )).toRequestBody(JSON)
+    fun searchIssues(
+        jql: String,
+        fields: List<String> = emptyList(),
+        expand: List<String> = emptyList(),
+        maxResults: Int = 100,
+        startAt: Int = 0,
+    ): SearchResults {
+        val payload = JiraSearchRequest(
+            expand = expand,
+            fields = fields,
+            jql = jql,
+            maxResults = maxResults,
+            startAt = startAt
+        )
 
-         val response = this.postRequest("/search", payload)
-         response.use { response ->
-             if (!response.isSuccessful) {
-                 throw IOException("Unexpected code $response")
-             }
-
-             return Json.decodeFromString<SearchResults>(response.body!!.string())
-         }
-     }
-
-    fun getProject(
-        key: String
-    ): Project {
-        val response = this.getRequest("/project/$key")
-        response.use { response ->
-            if (!response.isSuccessful) {
-                throw IOException("Unexpected code $response")
-            }
-
-            return this.JsonWithUnknownKeys.decodeFromString<Project>(response.body!!.string())
+        val response = jiraApi.searchIssues(payload).execute()
+        if (!response.isSuccessful) {
+            throw IOException("Unexpected code $response")
         }
+
+        return response.body() ?: throw IOException("Empty response body")
+    }
+
+    fun getProject(key: String): Project {
+        val response = jiraApi.getProject(key).execute()
+        if (!response.isSuccessful) {
+            throw IOException("Unexpected code ${response.code()}")
+        }
+
+        return response.body() ?: throw IOException("Empty response body")
+    }
+
+    fun getIssue(
+        key: String,
+        includedFields: String = "*all",
+        expand: String = "changelog",
+    ): IssueBean {
+        val response = jiraApi.getIssue(key, includedFields, expand).execute()
+        if (!response.isSuccessful) {
+            throw IOException("Unexpected code ${response.code()}")
+        }
+
+        return response.body() ?: throw IOException("Empty response body")
     }
 
 }

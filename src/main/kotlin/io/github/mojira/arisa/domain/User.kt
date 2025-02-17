315c75a4c3c8dd8597769864c9196cc2e91362fd
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2025-02-17 16:08:51 +0100

    fix: mark name as deprecated, add accountId

--- src/main/kotlin/io/github/mojira/arisa/domain/User.kt
+++ src/main/kotlin/io/github/mojira/arisa/domain/User.kt
@@ -1,6 +1,8 @@
 package io.github.mojira.arisa.domain
 
 data class User(
+    val accountId: String,
+    @Deprecated("Use `accountId` to identify users.")
     val name: String?,
     val displayName: String?,
     val getGroups: () -> List<String>?,

ddd2e7426dff779bf2b9aa48ca15090ece66a7bc
urielsalis <uriel@urielsalis.com>
2026-01-28 08:45:15 +0100

    Fix missing field in attachment
    Looks like some attachments are coming without a mimetype, causing the parsing to fail

--- src/main/kotlin/io/github/mojira/arisa/domain/Attachment.kt
+++ src/main/kotlin/io/github/mojira/arisa/domain/Attachment.kt
@@ -7,15 +7,17 @@ data class Attachment(
     val id: String,
     val name: String,
     val created: Instant,
-    val mimeType: String,
+    val mimeType: String?,
     val remove: () -> Unit,
     val openContentStream: () -> InputStream,
     val getContent: () -> ByteArray,
     val uploader: User?
 ) {
     /** Returns whether the type of the content is text */
-    fun hasTextContent() = mimeType.startsWith("text/") or
-        (mimeType == "application/json") or (mimeType == "application/xml")
+    fun hasTextContent() = mimeType != null && (
+        mimeType.startsWith("text/") or
+            (mimeType == "application/json") or (mimeType == "application/xml")
+        )
 
     /** Decodes the content as UTF-8 String */
     fun getTextContent() = String(getContent())
